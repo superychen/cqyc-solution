e5b4be67d478fbd6d9f941701ffa48a6afd7b557
cqyc <825467364@qq.com>
2022-01-06 23:45:24 +0800

    283

---
+++ src/main/java/com/cqyc/solution/MoveZeroes283.java
@@ -0,0 +1,31 @@
+package com.cqyc.solution;
+
+/**
+ * @Description:
+ * @author: cqyc
+ * @date 2022/1/6
+ */
+public class MoveZeroes283 {
+
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int res = nums[i];
+            if(res != 0) {
+                nums[i] = nums[j];
+                nums[j] = res;
+                j++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MoveZeroes283 moveZeroes283 = new MoveZeroes283();
+        int[] nums = new int[]{0,1,0,3,12};
+        moveZeroes283.moveZeroes(nums);
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+
+}
