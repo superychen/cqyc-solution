6c76c82db8c517d4fcb28ce68912f550df5afc16
yangchen <825467364@qq.com>
2022-09-23 18:04:34 +0800

    哎，太菜了

---
+++ src/main/java/com/cqyc/solution/CountPrimes_204.java
@@ -0,0 +1,54 @@
+package com.cqyc.solution;
+
+import java.util.Arrays;
+
+/**
+ * @author cqyc
+ * @create 2022-09-22-11:20
+ */
+public class CountPrimes_204 {
+
+    public int countPrimes(int n) {
+        if(n <= 2) {
+            return 0;
+        }
+
+        int res = 0;
+        for (int i = 2; i < n; i++) {
+            if (isCountPrimes(i)) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+    public boolean isCountPrimes(int count) {
+        for (int i = 2; i * i <= count; i++) {
+            if(count % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int countPrimes_two(int n) {
+        if(n < 2) {
+            return 0;
+        }
+        int[] resNum = new int[n];
+        Arrays.fill(resNum, 1);
+        int ans = 0;
+
+        for (int i = 2; i < n; i++) {
+            if(resNum[i] == 1) {
+                ans += 1;
+                if((long) i * i < n) {
+                    for (int j = i * i; j < n; j += i) {
+                        resNum[j] = 0;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
