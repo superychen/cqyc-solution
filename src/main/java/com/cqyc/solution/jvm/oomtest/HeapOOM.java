a9c2f519b48eaadd3a2dd309f0b81d328d5ead13
cqyc <825467364@qq.com>
2023-12-22 19:59:59 +0800

    oom

---
+++ src/main/java/com/cqyc/solution/jvm/oomtest/HeapOOM.java
@@ -0,0 +1,22 @@
+package com.cqyc.solution.jvm.oomtest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM Args：-Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * @author: CQYC
+ * @date: 2023-12-22-16:26
+ */
+public class HeapOOM {
+
+    static class OOMObject {
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<>();
+        while (true) {
+            list.add(new OOMObject());
+        }
+    }
+}
