0bc03698b497a9844f66dd2045c11be7f6073032
cqyc <825467364@qq.com>
2024-08-22 10:44:24 +0800

    一个快速排序和21题的合并两个有序链表

--- src/main/java/com/cqyc/solution/arr/MergeTwoLists_21.java
+++ src/main/java/com/cqyc/solution/arr/MergeTwoLists_21.java
@@ -7,7 +7,7 @@ package com.cqyc.solution.arr;
 public class MergeTwoLists_21 {
 
 
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
         ListNode() {}
@@ -27,6 +27,44 @@ public class MergeTwoLists_21 {
             list2.next = mergeTwoLists(list1, list2.next);
             return list2;
         }
+    }
+
+    public ListNode mergeTwoListsTwo(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(4);
+        ListNode listNode2 = new ListNode(1);
+        listNode2.next = new ListNode(3);
+        listNode2.next.next = new ListNode(4);
+
+        MergeTwoLists_21 test = new MergeTwoLists_21();
+        ListNode res = test.mergeTwoListsTwo(listNode, listNode2);
+        while (res != null) {
+            System.out.print(res.val + " ");
+            res = res.next;
+        }
+
     }
 
 
