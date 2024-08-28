278a2421037808c2abe195fda1ea5252a5ee4a39
chen.yang <825467364@qq.com>
2024-08-29 00:45:32 +0800

    test

--- src/main/java/com/cqyc/solution/arr/两数相加_2.java
+++ src/main/java/com/cqyc/solution/arr/两数相加_2.java
@@ -9,38 +9,42 @@ package com.cqyc.solution.arr;
 public class 两数相加_2 {
 
 
+    /**
+     * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+     *
+     * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+     *
+     * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+     */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode pre = new ListNode(0);
-        ListNode cur = pre;
-        //取它的进制位
-        int carry = 0;
+        ListNode cur = new ListNode(0);
+        ListNode prev = cur;
+        //给一个进位标识
+        int currentFlag = 0;
         while(l1 != null || l2 != null) {
-            //取总数
+            //计算两个数的和，并且要考虑进位
             int x = l1 == null ? 0 : l1.val;
             int y = l2 == null ? 0 : l2.val;
-            int sum = x + y + carry;
+            int sum = x + y + currentFlag;
 
-            //取除位，大于10就为1，<10就为0
-            carry = sum / 10;
-            //取余位
+            //取余数和整除数,这里最大为1，最小为0
+            currentFlag = sum / 10;
             sum = sum % 10;
-            cur.next = new ListNode(sum);
-            cur = cur.next;
 
+            prev.next = new ListNode(sum);
+            prev = prev.next;
             if(l1 != null) {
                 l1 = l1.next;
             }
-            if (l2 != null) {
+            if(l2 != null) {
                 l2 = l2.next;
             }
-            //考虑最后进一位
-            if(carry == 1) {
-                cur.next = new ListNode(carry);
+            //到最后考虑往后进一位
+            if(currentFlag == 1) {
+                prev.next = new ListNode(currentFlag);
             }
         }
-
-        return pre.next;
-
+        return cur.next;
     }
 
     public class ListNode {
